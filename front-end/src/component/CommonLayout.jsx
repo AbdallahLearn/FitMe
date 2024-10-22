d0cdaf338d8c64a7aae31abd17e7e09773ea3f1d
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-10-23 01:27:10 +0300

    updated in header

--- front-end/src/component/CommonLayout.jsx
+++ front-end/src/component/CommonLayout.jsx
@@ -6,7 +6,7 @@ import Footer from "./Footer";
 export function CommonLayout() {
   return (
     <div className="flex h-screen w-screen flex-col overflow-hidden">
-      <Header />
+      {/* <Header /> */}
       <div className="flex flex-1 flex-col gap-4 sm:gap-6 overflow-y-auto bg-[#EEE6E6]">
 
         <Outlet />
