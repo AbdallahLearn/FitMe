50f364edabba7cee2dc11016984902465fe42394
test <test@example.com>
2024-10-22 22:55:18 +0300

    update respons

--- front-end/src/pages/PasswordChanged.jsx
+++ front-end/src/pages/PasswordChanged.jsx
@@ -3,29 +3,27 @@ export default function PasswordChanged() {
         <>
             {/* Begin: Password Changed */}
             <div className="min-w-screen min-h-screen flex justify-center items-center bg-[#EEE6E6]">
-                <div className="min-w-full min-h-full flex flex-col justify-center items-center md:flex-row">
-                    <div className="w-full md:w-2/5 xl:w-1/2 flex justify-center">
-                        <h1 className="text-[#EE8B48] max-sm:text-7xl text-8xl font-bold text-center mb-10 md:mb-0">Fit Me</h1>
-                    </div>
-
-                    <div className="flex min-h-full flex-1 flex-col justify-center md:items-center xl:items-start w-full lg:w-1/2 px-10 md:pr-10 lg:pr-0">
-                        <div className="w-full lg:w-2/3 bg-[#1D1B1B] py-12 px-10 rounded-2xl">
+                <div className="min-w-full min-h-full flex flex-col justify-center items-center md:flex-row md:gap-20">
+                    <div className="flex min-h-full">
+                        <div className="w-full h-[80vh] md:w-[30rem] max-sm:gap-10 min-md-h-[90vh] bg-[#1D1B1B] px-10 rounded-2xl flex flex-col items-center justify-center">
                             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                                <h2 className="mt-10 mb-3 text-center max-sm:text-2xl md:text-4xl font-bold leading-9 tracking-tight text-white">
-                                    Reset Password 
+                                <h2 className="text-font text-center max-sm:text-2xl md:text-4xl font-bold leading-9 tracking-tight text-white">
+                                    Password Changed
                                 </h2>
 
-                                <p className="text-white max-sm:text-lg md:text-xl text-center">
-                                    Your password has been changed succesfully
-                                </p>
+                                <div className="w-full flex justify-center items-center max-sm:mt-8">
+                                    <p className="text-white max-sm:text- text-center w-72">
+                                        Your password has been changed succesfully
+                                    </p>
+                                </div>
                             </div>
 
-                            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-full">
+                            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-full flex flex-col items-center justify-center">
                                 <form method="POST" className="space-y-6">
-                                    <div>
+                                    <div className="flex flex-col justify-center items-center">
                                         <button
                                             type="submit"
-                                            className="flex w-full justify-center rounded-md bg-[#EE8B48] px-3 py-1.5 max-sm:text-lg md:text-2xl font-semibold leading-6 text-white shadow-sm hover:bg-[#EE8B30] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                                            className="flex w-80 h-12 justify-center items-center rounded-lg bg-[#EE8B48] px-3 py-1.5 max-sm:text-lg font-semibold leading-6 text-white shadow-sm hover:bg-[#EE8B30] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                                         >
                                             Back To Login
                                         </button>
--- front-end/src/pages/SignIn.jsx
+++ front-end/src/pages/SignIn.jsx
@@ -5,87 +5,73 @@ export default function SignIn() {
         <>
             {/* Begin: Sign In */}
             <div className="min-w-screen min-h-screen flex justify-center items-center bg-[#EEE6E6]">
-                <div className="min-w-full min-h-full flex flex-col justify-center items-center md:flex-row">
-                    <div className="w-full md:w-2/5 xl:w-1/2 flex justify-center">
-                        <h1 className="text-[#EE8B48] max-sm:text-7xl text-8xl font-bold text-center mb-10 md:mb-0">Fit Me</h1>
+                <div className="min-w-full min-h-full flex flex-col justify-center items-center md:flex-row md:gap-20">
+                    <div className="flex justify-center w-[30%] md:mr-10">
+                        <h1 className="text-[#EE8B48] title-font text-[8rem] md:text-[10rem] lg:text-[12rem] xl:text-[13rem] text-center mb-10 md:mb-0">FitMe</h1>
                     </div>
 
-                    <div className="flex min-h-full flex-1 flex-col justify-center md:items-center xl:items-start w-full lg:w-1/2 px-10 md:pr-10 lg:pr-0">
-                        <div className="w-full lg:w-2/3 bg-[#1D1B1B] py-12 px-10 rounded-2xl">
+                    <div className="flex min-h-full">
+                        <div className="w-full h-[80vh] md:w-[30rem] min-md-h-[90vh] bg-[#1D1B1B] px-10 rounded-2xl flex flex-col items-center justify-center">
                             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                                <h2 className="mt-10 text-center max-sm:text-2xl md:text-4xl font-bold leading-9 tracking-tight text-white">
+                                <h2 className="text-font text-center max-sm:text-2xl md:text-4xl font-bold leading-9 tracking-tight text-white">
                                     Sign In 
                                 </h2>
                             </div>
 
-                            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-full">
+                            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-full flex flex-col items-center justify-center">
                                 <form method="POST" className="space-y-6">
-                                    <div>
-                                        <label htmlFor="email" className="block max-sm:text-lg md:text-2xl font-medium leading-6 text-white">
+                                    <div className="flex flex-col justify-center items-center">
+                                        <label htmlFor="email" className="block max-sm:text-lg font-medium leading-6 text-white text-left w-80">
                                             Email address
                                         </label>
-                                        <div className="mt-2 relative">
+                                        <div className="mt-2 w-80">
                                             <input
                                                 id="email"
                                                 name="email"
                                                 type="email"
                                                 required
                                                 autoComplete="email"
-                                                className="pl-3 block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="pl-3 block w-80 h-12 rounded-lg border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                             />
-
-                                            
-                                        <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
-                                                <button type="button"  className="focus:outline-none border-2 rounded-full  bg-[#EE8B48] border-[#EE8B48]">
-                                                    <i className="fa-solid fa-check text-base fa-fw text-white"></i>
-                                                </button>
-                                            </div>
                                         </div>
                                     </div>
 
-                                    <div>
+                                    <div className="flex flex-col justify-center items-center">
                                         <div>
-                                            <label htmlFor="password" className="block max-sm:text-lg md:text-2xl font-medium leading-6 text-white">
+                                            <label htmlFor="password" className="block max-sm:text-lg font-medium leading-6 text-white text-left w-80">
                                             Password
                                             </label>
                                         </div>
 
-                                        <div className="mt-2 relative">
+                                        <div className="mt-2 w-80">
                                             <input
                                                 id="password"
                                                 name="password"
                                                 type="password"
                                                 required
                                                 autoComplete="current-password"
-                                                className="pl-3 block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="pl-3 block w-80 h-12 rounded-lg border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                             />
-
-                                            
-                                            <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
-                                                <button type="button"  className="focus:outline-none border-2 px-1 border-[#EE8B48]">
-                                                    <i className="fa-solid fa-eye text-lg fa-fw text-[#EE8B48]"></i>
-                                                </button>
-                                            </div>
                                         </div>
 
-                                        <div className="text-sm mt-2">
-                                            <Link to="/resetpassword" className="font-semibold text-white max-sm:text-xs md:text-sm">
+                                        <div className="text-sm mt-2 w-80">
+                                            <Link to="/resetpassword" className="font-medium text-white max-sm:text-xs md:text-sm">
                                                 Forgot password?
                                             </Link>
                                         </div>
                                     </div>
 
-                                    <div>
+                                    <div className="flex flex-col justify-center items-center">
                                         <button
                                             type="submit"
-                                            className="flex w-full justify-center rounded-md bg-[#EE8B48] px-3 py-1.5 max-sm:text-lg md:text-2xl font-semibold leading-6 text-white shadow-sm hover:bg-[#EE8B30] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                                            className="flex w-80 h-12 justify-center items-center rounded-lg bg-[#EE8B48] px-3 py-1.5 max-sm:text-lg font-semibold leading-6 text-white shadow-sm hover:bg-[#EE8B30] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                                         >
                                             Sign in
                                         </button>
                                     </div>
                                 </form>
 
-                                <p className="mt-10 text-center max-sm:text-xs md:text-sm text-gray-500">
+                                <p className="mt-5 text-center max-sm:text-xs md:text-base text-white">
                                     have{"'"}t an account?{" "}
                                     <Link to="/signup" className="font-semibold leading-6 text-[#EE8B48] hover:text-[#EE8B30]">
                                         Sign up
