01c4988174a14590639fab691be8273c0bca9da4
AbdallahLearn <abdallahjhn1@gmail.com>
2024-10-23 01:16:33 +0300

    link signup and signin with home

--- front-end/src/pages/SignIn.jsx
+++ front-end/src/pages/SignIn.jsx
@@ -7,7 +7,7 @@ export default function SignIn() {
             <div className="min-w-screen min-h-screen flex justify-center items-center bg-[#EEE6E6]">
                 <div className="min-w-full min-h-full flex flex-col justify-center items-center md:flex-row md:gap-20">
                     <div className="flex justify-center w-[30%] md:mr-10">
-                        <h1 className="text-[#EE8B48] title-font text-[8rem] md:text-[10rem] lg:text-[12rem] xl:text-[13rem] text-center mb-10 md:mb-0">FitMe</h1>
+                        <Link to='/home' className="text-[#EE8B48] title-font text-[8rem] md:text-[10rem] lg:text-[12rem] xl:text-[13rem] text-center mb-10 md:mb-0">FitMe</Link>
                     </div>
 
                     <div className="flex min-h-full">
--- front-end/src/pages/SignUp.jsx
+++ front-end/src/pages/SignUp.jsx
@@ -8,7 +8,7 @@ export default function SignUp() {
             <div className="min-w-screen min-h-screen flex justify-center items-center bg-[#EEE6E6]">
                 <div className="min-w-full min-h-full flex justify-center items-center flex-wrap gap-20">
                     <div className="flex justify-center w-[30%] md:mr-10">
-                        <h1 className="text-[#EE8B48] title-font text-[8rem] md:text-[10rem] lg:text-[12rem] xl:text-[13rem] text-center mb-10 md:mb-0">FitMe</h1>
+                        <Link to='/home' className="text-[#EE8B48] title-font text-[8rem] md:text-[10rem] lg:text-[12rem] xl:text-[13rem] text-center mb-10 md:mb-0">FitMe</Link>
                     </div>
 
                     <div className="flex min-h-full">
