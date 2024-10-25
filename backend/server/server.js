c612ffe93a66238995916264bf9473cc4f9f4374
Lama AlAjmah <ilamaabdullah77@gmail.com>
2024-10-25 19:46:28 +0300

    added .env file inside Server Folder

--- backend/server/server.js
+++ backend/server/server.js
@@ -7,15 +7,12 @@ import cors from 'cors';
 import connectDB from "./config/db.js"; 
 
 
-
-
 dotenv.config();
-
 console.log(process.env.MONGO_URI); // طباعة قيمة MONGO_URI للتحقق
-
 
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -25,14 +22,11 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 
-
 connectDB();
-
 // async function main() {
 //     await mongoose.connect(process.env.MONGO_URI);
 //     console.log('MongoDB connected');
 // }
-
 // main().catch(err => console.log(err));
 
 app.listen(port,()=>{
