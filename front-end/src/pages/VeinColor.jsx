52b0b46d54006cf07b7802d33b55bde61a42047f
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-10-27 15:05:04 +0300

    fix layout in veinsColor

--- front-end/src/pages/VeinColor.jsx
+++ front-end/src/pages/VeinColor.jsx
@@ -15,11 +15,11 @@ function VeinColor({ onNext }) {
       <h2 className='mb-4 text-gray-500 text-center max-sm:text-[0.8rem]'><span className=' text-[#EE8B48] font-bold'>Hint:</span> Look at the veins on your wrists</h2>
       <div className="flex flex-wrap justify-center gap-8">
         <div 
-          className={`box rounded-lg text-center cursor-pointer px-4 p-2 flex flex-col items-center ${selected === 'warm' ? 'border-4 border-green-500' : 'border border-gray-400'}`}
+          className={`box rounded-lg text-center cursor-pointer px-8 p-2 flex flex-col items-center ${selected === 'warm' ? 'border-4 border-green-500' : 'border border-gray-400'}`}
           onClick={() => handleSelect('warm')}
         >
-          <h1 className="text-base sm:text-lg">WARM</h1>
-          <p className="pt-1 sm:pt-2 text-xs sm:text-sm">Green veins</p>
+          <h1 className="text-base sm:text-lg font-extrabold">WARM</h1>
+          <p className="-mt-1 text-xs sm:text-sm">Green veins</p>
           <div className="circle-container flex gap-2 pt-2 justify-center">
             <div className="circle w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-[#5E887A]"></div>
             <div className="circle w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-[#6E7F5C]"></div>
@@ -27,11 +27,11 @@ function VeinColor({ onNext }) {
         </div>
 
         <div 
-          className={`box rounded-lg text-center cursor-pointer px-4 p-2 flex flex-col items-center ${selected === 'cool' ? 'border-4 border-blue-500' : 'border border-gray-400'}`}
+          className={`box rounded-lg text-center cursor-pointer px-8 p-2 flex flex-col items-center ${selected === 'cool' ? 'border-4 border-blue-500' : 'border border-gray-400'}`}
           onClick={() => handleSelect('cool')}
         >
-          <h1 className="text-base sm:text-lg">COOL</h1>
-          <p className="pt-1 sm:pt-2 text-xs sm:text-sm">Blue veins</p>
+          <h1 className="text-base sm:text-lg font-extrabold">COOL</h1>
+          <p className="-mt-1 text-xs sm:text-sm">Blue-green veins</p>
           <div className="circle-container flex gap-2 pt-2 justify-center">
             <div className="circle w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-[#728A9B]"></div>
             <div className="circle w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-[#539AAB]"></div>
@@ -39,11 +39,11 @@ function VeinColor({ onNext }) {
         </div>
 
         <div 
-          className={`box rounded-lg text-center cursor-pointer px-4 p-2 flex flex-col items-center ${selected === 'neutral' ? 'border-4 border-gray-500' : 'border border-gray-400'}`}
+          className={`box rounded-lg text-center cursor-pointer px-8 p-2 flex flex-col items-center ${selected === 'neutral' ? 'border-4 border-gray-500' : 'border border-gray-400'}`}
           onClick={() => handleSelect('neutral')}
         >
-          <h1 className="text-base sm:text-lg">NEUTRAL</h1>
-          <p className="pt-1 sm:pt-2 text-xs sm:text-sm">Mixed veins</p>
+          <h1 className="text-base sm:text-lg font-extrabold">NEUTRAL</h1>
+          <p className="-mt-1 text-xs sm:text-sm">Mixed veins</p>
           <div className="circle-container flex gap-2 pt-2 justify-center">
             <div className="circle w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-[#7B729B]"></div>
             <div className="circle w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-[#537BAB]"></div>
